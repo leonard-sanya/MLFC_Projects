f7542f80a5411ea2edd570b8b5c7c031d5af1a9b
Leonard <lsanya@aimsammi.org>
2025-09-07 04:04:48 +0300

    updated the plot_city_map function

--- fynesse/access.py
+++ fynesse/access.py
@@ -1,13 +1,27 @@
 import osmnx as ox
 
+
+
 def get_osm_datapoints(latitude, longitude, box_size_km=2, poi_tags=None):
+
+    tags = {
+    "amenity": True,
+    "buildings": True,
+    "historic": True,
+    "leisure": True,
+    "shop": True,
+    "tourism": True,
+    "religion": True,
+    "memorial": True}
+
+
     box_width = box_size_km / 111
     box_height = box_size_km / 111
     north = latitude + box_height
     south = latitude - box_height
     west = longitude - box_width
     east = longitude + box_width
-    tags = poi_tags or {}
+    # tags = poi_tags or {}
 
     try:
         if tuple(map(int, ox.__version__.split('.')))[0] >= 2:
