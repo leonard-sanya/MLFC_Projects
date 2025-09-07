65fb52056e1df536a3db9eea4c1db3f7135e8e89
Leonard <lsanya@aimsammi.org>
2025-09-07 04:44:10 +0300

    updated the plot_city_map function

--- fynesse/access.py
+++ fynesse/access.py
@@ -21,12 +21,12 @@ def get_osm_datapoints(latitude, longitude, box_size_km=2, poi_tags=None):
     east = longitude + box_width
     # tags = poi_tags or {}
     print("working")
-    try:
-        bbox = (west, south, east, north)
-        pois = ox.features_from_bbox(bbox, poi_tags=tags)
-        return pois
-    except Exception as e:
-        print(f"[Warning] OSM query failed: {e}")
-        return None
+    # try:
+    #     bbox = (west, south, east, north)
+    #     pois = ox.features_from_bbox(bbox, poi_tags=tags)
+    #     return pois
+    # except Exception as e:
+    #     print(f"[Warning] OSM query failed: {e}")
+    #     return None
 
 
